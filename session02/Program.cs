ef70658a7b76383d629e1fbd7a0c02201054819d
tarik31osama-afk <tarik31osama@gmail.com>
2026-09-05 02:24:25 +0300

    1st q

--- session02/Program.cs
+++ session02/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace session02
+﻿using System.Globalization;
+
+namespace session02
 {
     internal class Program
     {
+        #region 1st q
+        class Book
+        {
+          public  string title;
+            public int pages;
+         
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book=new Book();
+            book.title = "c# basics";
+            book.pages = 500;
+
+            object obj = book;
+            Console.WriteLine(obj);
+      
         }
+        #endregion
     }
 }
